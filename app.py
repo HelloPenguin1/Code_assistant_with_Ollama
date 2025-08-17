e7dc6cb22ff48449c0f016b7b9d9c7b067d90be0
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-08-17 18:57:40 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import requests
+import gradio as gr
+import json
+
+
+url = "http://localhost:11434/api/generate" 
+
+headers = {
+    'Content-Type': "application/json"
+}
+
+
+history = []
+def gen_response(prompt):
+
+    history.append(prompt)
+    final_prompt = "\n".join(history)
+
+    data = {
+        "model":"CodeJarvis",
+        "prompt":final_prompt,
+        "stream" : False
+    }
+
+    response = requests.post(
+        url = url,
+        headers=headers,
+        data=json.dumps(data)
+    )
+
+    if response.status_code==200:
+        response = response.text
+        data = json.loads(response)
+        actual_response = data['response']
+        return actual_response
+    else:
+        print("error:", response.text)
+
+
+interface = gr.Interface(
+    fn = gen_response,
+    inputs = gr.Textbox(lines = 4, placeholder = "Enter your prompt"),
+    output = "text"
+)
